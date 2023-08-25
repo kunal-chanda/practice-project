edfe5c17a9d36630252f39a9e3388c31ab3e042b
Kunal <kunalrcciit@gmail.com>
2023-08-25 16:43:46 +0530

    25.08.23_16.43

---
+++ src/components/Error.js
@@ -0,0 +1,12 @@
+import '../App.css';
+import React from 'react'
+
+const Error = () => {
+  return (
+    <div className='errorDiv'>
+      <h1>404 !! NOT FOUND</h1>
+    </div>
+  )
+}
+
+export default Error
